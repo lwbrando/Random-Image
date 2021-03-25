b0c5e0f824ff288431a7451c29f47de2cdc22720
RanieriC <51229092+RanieriC@users.noreply.github.com>
2021-03-25 16:18:27 +0100

    Update Random Image.bat

--- Random Image.bat
+++ Random Image.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Random Image v0.1a by Brando
 ping 127.0.0.1 -n 3 > nul
-node C:\Users\braba\Documents\ciao\ciaoo\root\index.js
-C:\Users\braba\Documents\ciao\ciaoo\immagine.jpg
+node index.js
+immagine.jpg
